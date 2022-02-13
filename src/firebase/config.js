002d476a50e2970f6a59bcd1e93160e06ddf7249
ciamac-da <ciamac.davoudi@gmail.com>
2022-02-13 12:40:00 +0100

    Verified by Marti4N v1.0.3 firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBfys4FJ8tjxf0e0YIBC1r0v1YCOVj86dQ",
+    authDomain: "vue-chat-room-46d74.firebaseapp.com",
+    projectId: "vue-chat-room-46d74",
+    storageBucket: "vue-chat-room-46d74.appspot.com",
+    messagingSenderId: "1076960969318",
+    appId: "1:1076960969318:web:097d186854a22ebdc2e3a7",
+    measurementId: "G-3YN186X96M"
+  };
+
+// Init firebase
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, timestamp}
