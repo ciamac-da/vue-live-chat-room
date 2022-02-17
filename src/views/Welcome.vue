6b3dcd84b320b2eea34c5b8d037d59a1b4480f8f
ciamac-da <ciamac.davoudi@gmail.com>
2022-02-17 13:56:32 +0100

    Verified by Marti4N v1.0.4 Signup Signin Form added

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,15 +1,41 @@
 <template>
     <div class="welcome container">
         <p>Welcome</p>
+        <SignupForm />
+        <LoginForm />
     </div>
 </template>
 
 <script>
+import SignupForm from "../components/SignupForm.vue"
+import LoginForm from "../components/LoginForm.vue"
+
+export default {
+    components: { SignupForm, LoginForm }
+}
 </script>
 
-<style scoped>
+<style>
  .welcome {
      text-align: center;
      padding: 20px 0;
  }
+ .welcome form {
+     width: 300px;
+     margin: 20px auto;
+ }
+ .welcome label {
+     display: block;
+     margin: 20px 0 10px;
+ }
+ .welcome input {
+     width: 100%;
+     margin: 10px auto;
+     color: #999;
+     outline: none;
+     border: 1px solid #eee;
+     border-radius: 20px;
+     padding: 10px;
+ }
+
 </style>
