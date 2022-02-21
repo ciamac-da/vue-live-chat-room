ac4e26091cf006aab4d3035e3c7e6f5c4f246211
ciamac-da <ciamac.davoudi@gmail.com>
2022-02-21 08:52:16 +0100

    Verified by Marti4N v1.0.5 switching between forms

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,17 +1,31 @@
 <template>
     <div class="welcome container">
         <p>Welcome</p>
-        <SignupForm />
-        <LoginForm />
+        <div v-if="showLogin">
+            <h2>Login</h2>
+            <LoginForm />
+            <p>No account yet? <span @click="showLogin = false">Signup</span> instead</p>
+        </div>
+        <div v-else>
+            <h2>Sign up</h2>
+            <SignupForm />
+            <p>Already registered? <span @click="showLogin = true">Login</span></p>
+        </div>
     </div>
 </template>
 
 <script>
 import SignupForm from "../components/SignupForm.vue"
 import LoginForm from "../components/LoginForm.vue"
+import { ref } from "vue"
 
 export default {
-    components: { SignupForm, LoginForm }
+    components: { SignupForm, LoginForm },
+    setup() {
+        const showLogin = ref(true)
+
+        return { showLogin }
+    }
 }
 </script>
 
@@ -19,15 +33,15 @@ export default {
  .welcome {
      text-align: center;
      padding: 20px 0;
- }
+     }
  .welcome form {
      width: 300px;
      margin: 20px auto;
- }
+     }
  .welcome label {
      display: block;
      margin: 20px 0 10px;
- }
+      }
  .welcome input {
      width: 100%;
      margin: 10px auto;
@@ -36,6 +50,13 @@ export default {
      border: 1px solid #eee;
      border-radius: 20px;
      padding: 10px;
- }
-
+     }
+ .welcome span {
+     cursor: pointer;
+     font-weight: bolder;
+     text-decoration: underline;
+     }
+ .welcome span {
+     margin: 20px auto;
+     }
 </style>
