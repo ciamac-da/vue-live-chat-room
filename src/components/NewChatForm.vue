3c844bde18a6166789f12aabf9ed24089fd6c573
ciamac-da <ciamac.davoudi@gmail.com>
2022-04-07 22:54:20 +0200

    Verified by Marti4n v2.1.1 adding new messages to firestore

--- src/components/NewChatForm.vue
+++ src/components/NewChatForm.vue
@@ -1,44 +1,46 @@
 <template>
-    <form>
-        <textarea
-        placeholder="Type a message and hit enter to send..."
-        v-model= "message"
-        @keypress.enter.prevent= "handleSubmit"
-        >
-        </textarea>
-    </form>
+  <form>
+    <textarea
+      placeholder="Type a message and hit enter to send..." 
+      v-model="message"
+      @keypress.enter.prevent="handleSubmit"
+    ></textarea>
+    <div v-if="error" class="error">{{ error }} blah</div>
+  </form>
 </template>
 
 <script>
-import { ref } from "vue"
-import getUser from "../composables/getUser"
+import useCollection from '../composables/useCollection'
+import getUser from '../composables/getUser'
 import { timestamp } from '../firebase/config'
-
+import { ref } from 'vue'
 export default {
-    setup() {
-        const { user } = getUser()
-
-        const message = ref("")
-
-        const handleSubmit = async () => {
-            const chat = {
-                name: user.value.displayName,
-                message: message.value,
-                createdAt: timestamp()
-            }
-            console.log(chat)
-            message.value= ""
-        }
-        return { message, handleSubmit }
+  setup() {
+    const { user } = getUser()
+    const { addDoc, error } = useCollection('messages')
+    // refs
+    const message = ref('')
+    const handleSubmit = async () => {
+      const chat = {
+        name: user.value.displayName,
+        message: message.value,
+        createdAt: timestamp()
+      }
+      await addDoc(chat)
+      if (!error.value) {
+        message.value = ''
+      }
     }
+    return { message, handleSubmit, error }
+  }
 }
 </script>
 
 <style scoped>
-form {
+  form {
     margin: 10px;
-}
-textarea {
+  }
+  textarea {
     width: 100%;
     max-width: 100%;
     margin-bottom: 6px;
@@ -48,5 +50,11 @@ textarea {
     border-radius: 20px;
     font-family: inherit;
     outline: none;
-}
+  }
+  .error {
+    text-align: center;
+    color: #ff2a58;
+    font-size: 12px;
+    padding: 10px 0;
+  }
 </style>
